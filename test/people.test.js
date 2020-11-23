e2d9b60f5e4bf6ac047b89df6f2a9873369a385d
willys_tjiawi <willys@huubap.com>
2020-11-24 00:44:45 +0800

    specify ErrorType of `truffle-assertions`

--- test/people.test.js
+++ test/people.test.js
@@ -6,6 +6,11 @@ contract('People', async function() {
         let instance = await People.deployed();
         // `createPerson` should fail if it is create with age more than 150
         // if it is not fail, there is bug
-        await truffleAssert.fails(instance.createPerson('Bob', 200, 170, {value: web3.utils.toWei('1', 'ether')}));
+        await truffleAssert.fails(
+            instance.createPerson('Bob', 200, 170, {value: web3.utils.toWei('1', 'ether')}),
+            truffleAssert.ErrorType.REVERT  // you can specify the expected error, in this case we expect REVERT since `require` fails (the correct behavior)
+            );
+
+            // if the type of error is not REVERT, for example, it is OUT_OF_GAS, meaning there is bug
     });
 });
